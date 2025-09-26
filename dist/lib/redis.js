d07b02a879de95cb59e8de45be9aa4f5bfd1b738
ArmanShaikh05 <arman.shaikh01082003@gmail.com>
2025-09-27 01:59:53 +0530

    minor

--- dist/lib/redis.js
+++
@@ -1,9 +0,0 @@
-import Redis from "ioredis";
-import env from "../config/config.js";
-export const redis = new Redis.default({
-    host: env.REDIS_HOST,
-    port: Number(env.REDIS_PORT),
-    password: env.REDIS_PASSWORD,
-});
-export default redis;
-//# sourceMappingURL=redis.js.map
